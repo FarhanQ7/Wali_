f0da571f90082d8d61bed517722c049626a428e7
Farhan Qureshi <Farhan.q@live.com>
2018-07-18 19:41:30 -0400

    Wali_/

---
+++ Wali/Wali/WaliPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace Wali
+{
+	public partial class WaliPage : ContentPage
+	{
+		public WaliPage()
+		{
+			InitializeComponent();
+
+					var webView = new WebView 
+		  {
+		     Source = "/Users/ffquresh/Projects/Wali/Wali/rootpage.html",
+		     VerticalOptions = LayoutOptions.FillAndExpand,
+		     HorizontalOptions = LayoutOptions.FillAndExpand
+		  };
+
+			Content = webView;
+
+
+
+		}
+	}
+}
